36ffcf2c82661148c58466f8f57efceed830ca57
Cloud User <centos@ip-172-31-28-192.ap-southeast-1.compute.internal>
2020-06-10 12:54:29 +0000

    A complete solution to Ex4.

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,4 +15,36 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+//define ports
+module dice(
+	input clk,
+	input rst,
+	input button,
+	output [2:0]throw
+	);
+//define registers
+	reg [2:0]throw;
+//logics
+	always@(posedge clk)
+	begin
+	 if (rst) 
+	 begin
+	 throw<=3'b000;
+	 end
+	 else if ((throw==3'b000)|(throw==3'b111))
+	 begin
+	 throw<=3'b001;
+	 end
+	 else if ((throw==3'b110)&(button==1))
+	 begin
+	 throw<=3'b001;
+	 end
+	 else 
+	 begin
+	 throw<=(button==1)?throw+3'b001:throw;
+	 end
+	end
+endmodule
+	 
 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,74 @@
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Parameters
+	parameter CLK_PERIOD = 10;
+
+//Registers and wires
+	reg clk;
+	reg rst;
+	reg button;
+	reg err;
+	reg [2:0]throw_prev;
+	wire [2:0]throw;
+
+//Clock generation
+	initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+//User logic
+	initial
+   begin
+	clk=0;
+	rst=0;
+	button=1;
+	err=0;
+	#10
+	forever
+	begin
+	#10
+	throw_prev=3'b000;
+	//check reset
+	if ((rst)&(throw!=3'b000)) begin
+	$display("***RESET FAILED! throw==%d, throw_prev==%d, rst='%d', button=%d ***",throw,throw_prev,rst,button);
+	err=1;
+	end
+	//check logic
+	if ((throw_prev!=3'b110)|(throw_prev!=3'b110)|(throw_prev!=3'b000)) begin
+	if ((button)&(throw!=throw_prev+3'b001)|(!button)&(throw!=throw_prev-3'b001)) begin
+	$display("***BUTTON FAILED! throw==%d, throw_prev==%d, rst='%d', button=%d ***",throw,throw_prev,rst,button);
+	err=1;
+	end
+	end
+	throw_prev=throw;
+	button=~button;
+	end
+   end
+
+//check for success
+	initial begin
+	 #100 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+        end
+
+//instantiate dice module
+	dice top(
+		
+		.clk(clk),
+		.rst(rst),
+		.button(button),		
+		.throw(throw)
+	);
+endmodule 
 
